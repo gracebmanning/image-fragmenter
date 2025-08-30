252c472ee588d7675bed62a12e186cb16b3ae0b7
Grace Manning <gracebmanning@gmail.com>
2025-08-30 15:21:11 -0700

    create-react-app files

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import './index.css';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  </BrowserRouter>
+);
