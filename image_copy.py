5b5e56ef830968d172658693fd1c1e5e0809325a
Grace Manning <gracebmanning@gmail.com>
2025-08-30 13:59:04 -0700

    starting Python script

---
+++ image_copy.py
@@ -0,0 +1,50 @@
+import argparse
+from PIL import Image
+import random
+import string
+import os
+import imageio.v3 as iio
+
+NUM_LOOPS = 40
+DURATION = 5
+
+
+def create_images(image_filepath):
+    original = Image.open(image_filepath)
+    img = original.copy()
+    filenames = []
+
+    dir_name = ''.join(random.choices(
+        string.ascii_letters + string.digits, k=8))
+    os.mkdir(dir_name)
+
+    img.save(f'./{dir_name}/img0.jpg')
+    filenames.append(f'./{dir_name}/img0.jpg')
+
+    width, height = img.size
+
+    for i in range(NUM_LOOPS):
+        left = random.randint(0, width)
+        top = random.randint(0, height)
+        right = left + random.randint(0, (width-left))
+        bottom = top + random.randint(0, (height-top))
+
+        crop = original.crop((left, top, right, bottom))
+        img.paste(crop, (random.randint(0, width), random.randint(0, height)))
+        filename = f'./{dir_name}/img{i+1}.jpg'
+        img.save(filename)
+        filenames.append(filename)
+
+    images = []
+    for file in filenames:
+        images.append(iio.imread(file))
+
+    iio.imwrite(f"./{dir_name}/animation.gif", images, duration=500, loop=0)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="imageCropReplicator")
+    parser.add_argument("image_file", help="Path to the input image file.")
+    args = parser.parse_args()
+    image_filepath = args.image_file
+    create_images(image_filepath)
