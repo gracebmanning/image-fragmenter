9f57a1183fcad1e6c690921d22602a2110b47954
Grace Manning <gracebmanning@gmail.com>
2025-09-15 20:50:15 -0700

    footer font styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,15 @@ import globe from "../assets/internet_connection_wiz-0.png";
 
 export default function Footer() {
     return (
-        <footer className="w-full font-sans text-base bg-neutral-300 text-neutral-600 flex flex-col items-center justify-center p-2">
-            <p className="mb-1">
+        <footer style={{ fontFamily: "Pixelated MS Sans Serif" }} className="w-full bg-neutral-300 text-neutral-800 flex flex-col items-center justify-center p-2">
+            <p className="text-sm mb-1">
                 Created by{" "}
                 <a href="https://graceis.online/" target="_blank" rel="noreferrer" className="underline">
                     Grace Manning
                 </a>
                 .
             </p>
-            <p className="mb-1">
+            <p className="text-sm mb-1">
                 Enjoyed it? Send a{" "}
                 <a href="https://ko-fi.com/graceisonline" target="_blank" rel="noreferrer" className="underline">
                     thank you
